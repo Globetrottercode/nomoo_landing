963745568d237ce025333cd936bcf930852aa4b1
MD Suweb Reza <swebreza@gmail.com>
2025-06-22 21:17:47 +0530

    added ImpactTrackingSection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import GreenChoicesSection from '../components/greenChoicesSection'
 import HeroSection from '../components/hero'
+import ImpactTrackingSection from '../components/ImpactTrackingSection'
 
 export default function Home() {
   return (
     <div className='bg-white h-screen'>
       <HeroSection />
       <GreenChoicesSection />
+      <ImpactTrackingSection />
     </div>
   )
 }
